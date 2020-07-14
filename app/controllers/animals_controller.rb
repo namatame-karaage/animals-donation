d8ffe177e93a8585196d62656e436a59bfe757c6
namatame-karaage <mariypuuu0519@gmail.com>
2020-07-14 12:01:06 +0900

    add_secutriy_fuction

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,13 +1,51 @@
 class AnimalsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @animals = Animal.all
   end
 
   def show
+    @animal = Animal.find(params[:id])
   end
 
   def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user_id = current_user.id
+    if @animal.save
+      redirect_to animal_path(@animal) , notice: '投稿に成功'
+    else
+      render :new
+    end
   end
 
   def edit
+    @animal = Animal.find(params[:id])
+    if @animal.user != current_user
+      redirect_to animals_path, alert: 'ダメ絶対！エラー'
+    end
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animal_path(@animal),notice:'更新に成功'
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animals_path
+  end
+  private
+  def animal_params
+    params.require(:animal).permit(:title, :body, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   attachment :profile_image
+
+  has_many :animals, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 end
 
